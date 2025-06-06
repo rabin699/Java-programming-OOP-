01f769d94e882fdc5e30537777ab56ce0ba12f63
rabin699 <rabinhumagain444@gmail.com>
2025-06-06 22:21:47 +0545

    added

---
+++ IsPrime.java
@@ -0,0 +1,20 @@
+
+//Write a program to read number and identify whether the given number is a prime number or not.
+import java.util.Scanner;
+
+public class IsPrime {
+    public static void main(String[] args) {
+        System.out.print("Enter a number:");
+        Scanner scn = new Scanner(System.in);
+        int num, i;
+        num = scn.nextInt();
+        for (i = 2; i <= num / 2; i++) {
+            if (num % i == 0) {
+                System.out.println("It is composite");
+                return;
+            }
+        }
+        System.out.println("It is prime");
+        scn.close();
+    }
+}
