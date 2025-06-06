639fd804316f2b017decbdb56eb0cf583364b8ba
rabin699 <rabinhumagain444@gmail.com>
2025-06-06 22:37:11 +0545

    added

--- Compare_a_b_c.java
+++ Compare_a_b_c.java
@@ -1,21 +1,25 @@
 
-//compariing the three values a,b,c
+// compariing the three values a,b,c
 import java.util.Scanner;
 
 public class Compare_a_b_c {
-    public static void main() {
+    public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
-        int a, b, c;
-        System.out.println("Enter three numbers:");
-        a = scn.nextInt();
-        b = scn.nextInt();
-        c = scn.nextInt();
-        if (a > b && a > c)
-            System.out.println(a + " is greater");
-        else if (b > c)
-            System.out.println(b + " is greater");
-        else
-            System.out.println(c + " is greater");
+        System.out.print("Enter first number: ");
+        int a = scn.nextInt();
+        System.out.print("Enter second number: ");
+        int b = scn.nextInt();
+        System.out.print("Enter third number: ");
+        int c = scn.nextInt();
+
+        if (a > b && a > c) {
+            System.out.println("a is the largest: " + a);
+        } else if (b > a && b > c) {
+            System.out.println("b is the largest: " + b);
+        } else {
+            System.out.println("c is the largest: " + c);
+        }
+
         scn.close();
     }
 }
